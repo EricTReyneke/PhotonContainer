4005c326bcc193a9d828e6bf78b8444271c6a7f4
Eric Theodore Reyneke <eric2001reyneke@gmail.com>
2024-06-17 19:18:40 +0200

    Deleted the PhotonContainer.Business.Data DLL

--- PhotonContainer.Business.Data/Interfaces/ITestStrings.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PhotonContainer.Business.Data.Interfaces
-{
-    public interface ITestStrings
-    {
-        string StringToReverse { get; set; }
-    }
-}
--- PhotonContainer.Business.Data/TestStrings/TestStringTwo.cs
+++
@@ -1,9 +0,0 @@
-﻿using PhotonContainer.Business.Data.Interfaces;
-
-namespace PhotonContainer.Business.Data.TestStrings
-{
-    public class TestStringTwo : ITestStrings
-    {
-        public string StringToReverse { get; set; } = "Lekker my maat die is die tweede string om te reverse. 2";
-    }
-}
